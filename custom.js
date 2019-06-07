13c68c481dd8f79c8b7646e5ea7ec868806ad782
gonzz007 <gonzo.daniel.77@gmail.com>
2019-06-06 17:49:28 -0700

    updated page and university,contact html created

---
+++ custom.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    $('.ui .item').on('click', function () {
+        $('.ui .item').removeClass('active');
+        $(this).addClass('active');
+    });
+})
