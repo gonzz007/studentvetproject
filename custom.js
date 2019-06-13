6e91f61285f9905332300aae8dcabe96cf8b6099
gonzz007 <gonzo.daniel.77@gmail.com>
2019-06-12 23:11:59 -0700

    API gets name and city

--- custom.js
+++ custom.js
@@ -1,16 +1,38 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-    $('.ui .item').on('click', function () {
-        $('.ui .item').removeClass('active');
-        $(this).addClass('active');
-    });
-})
-function collegelist(){
+//     $('.ui .item').on('click', function () {
+//         $('.ui .item').removeClass('active');
+//         $(this).addClass('active');
+//     });
+// })
+window.onload = function() {
+    var school = document.querySelector('#schoolName')
+let form=document.querySelector("#uni")
+let name = document.querySelector('#nameCollege')
+let collegeLocation= document.querySelector('#collegeLocation')
+let collegeurl = document.querySelector('#collegewebsite')
+let val
+let schoolInfo
+form.addEventListener("submit", function (event){ 
+    event.preventDefault()  
     
+    var key = "6cIJQHwVG2gQoWzAAn2DMbNsTde4DlCR8VENo6Zl";
+    val = school.value;
+    console.log(val)
+    //using Fetch to grab data from OWM API
+    fetch("https://api.data.gov/ed/collegescorecard/v1/schools?school.name=" + val + "&api_key=" + key)
+    .then(function(resp) { return resp.json() })
+    .then(function(data) {
+        
+        schoolInfo = data.results[0].school
+        
+        name.innerHTML = schoolInfo.name
+        collegeLocation.innerHTML=schoolInfo.city
+        collegeurl.innerHTML=schoolInfo.school_url
+        
+    .catch(function() {
+    }) ;
+})
+})
 }
-<script>
-var myObj = { "name":"John", "age":31, "city":"New York" };
-var myJSON = JSON.stringify(myObj);
-window.location = "demo_json.php?x=" + myJSON;
-</script>
